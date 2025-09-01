bd99cb4743e41799855be35999d1d71a33509eab
Nathan Malier <namalier@f1br6s11.clusters.42paris.fr>
2025-09-01 14:00:10 +0200

    Correction 02

--- 02/ex02/main.cpp
+++ 02/ex02/main.cpp
@@ -10,15 +10,33 @@ int main( void )
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
+	std::cout << --a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a-- << std::endl;
+	std::cout << a << std::endl;
+	std::cout << "b is : " << b << std::endl;
 
-	std::cout << b << std::endl;
-	std::cout << a - b << std::endl;
-	std::cout << a + b << std::endl;
-	std::cout << 8 / 2 << std::endl;
-	std::cout << 2 * 2 << std::endl;
+	std::cout << std::endl;
+	std::cout << std::endl;
 
-	std::cout << Fixed::max(a, b) << std::endl;
-	std::cout << Fixed::min(a, b) << std::endl;
+	Fixed c(1);
+	Fixed d(2);
+
+	std::cout << "Min is 1 and we have : " <<  Fixed::min(c, d) << std::endl;
+	std::cout << "Max is 2, and we have : " << Fixed::max(c, d) << std::endl;
+	std::cout << std::endl;
 	
+	std::cout << "2 / 1 = " << d / c << std::endl;
+	std::cout << "2 * 2 = " << d * d << std::endl;
+	std::cout << "1 - 2 = " << c - d << std::endl;
+	std::cout << "1 + 2 = " << c + d << std::endl;
+
+	if (c != d)
+		std::cout << "c is different than d" << std::endl;
+	Fixed e(d);
+	if (e == d)
+		std::cout << "e is equal to d" << std::endl;
+	if (e != d)
+		std::cout << "Should not appear" << std::endl;
 	return (0);
 }
